91e88bfbbbc47f41163e878745b7b9fd5bfe0297
Marco Carvalho de Pinho <marcocpinho@hotmail.com>
2024-01-12 11:56:11 -0300

    Aula

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,24 +1,9 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { useSelector, TypedUseSelectorHook } from "react-redux";
 
-const todoSilce = createSlice({
-  name: "todo",
-  initialState: ["Fazer café", "Estudar Redux"],
-
-  reducers: {
-    add: (state, action) => {
-      state.push(action.payload.newTodo);
-    },
-  },
-});
-
 export const store = configureStore({
-  reducer: {
-    todo: todoSilce.reducer,
-  },
+  reducer: {},
 });
-
-export const { add } = todoSilce.actions;
 
 export type RootState = ReturnType<typeof store.getState>;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
